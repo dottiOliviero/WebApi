62f965d2a850063cc6d450b198f946c77ab31fe4
dottiOliviero <49671014+dottiOliviero@users.noreply.github.com>
2020-03-03 22:24:29 +0100

    handle response

--- README.md
+++ README.md
@@ -1,35 +1,56 @@
-# Web Api Get Request
+# Web Api Converter Request
 
 First, open the commando prompt.
+
 Then you need to clone the git repository:
-' $ git clone git@github.com:'
+```
+$ git clone git@github.com:dottiOliviero/WebApi.git
+```
 Or:
-' $ git clone https://github.com/0'
+```
+$ git clone https://github.com/dottiOliviero/WebApi
+```
 
 Then change into the WebApi folder:
 
-'cd WebApi'
+```
+$ cd WebApi
+```
 
 Now, you need to create a virtual environment
-and install all the required packages listed in the requirements.txt file
+and install all the required packages listed in the *requirements.txt* file
+
+You can use pip+virtualenv:
+```
+$ pip install virtualenv  #in case you don't have it 
+$ virtualenv venv
+$ .venv/bin/activate # on Windows use "venv\Scripts\activate"
+$ pip install -r requirements.txt
+```
 
 ### Run the example
 
 To run the application you need to setup Flask. In the command prompt type the following commands:
-' $ set FLASK_APP = main.py  $ flask run'
+```
+$ set FLASK_APP = main.py  
+$ flask run
+```
 
-you should see from your console that the application is running on http://127.0.0.1:5000.
+you can see from your console that the application is running on http://127.0.0.1:5000.
 
-Open a browser and type http://127.0.0.1:5000, it should appear a blank page which says "welcome to the rates converter"
+Open a browser and type http://127.0.0.1:5000, it appears a blank page which says "Welcome to the currency converter".
+
 To perform a value conversion, you have to indicate the amount, the source currency (src_currency), the destination currency (dest_currency) and the reference time for the change rate (reference_date).
 The endpoint is called _convert_, hence the call to the source code should have the following structure:
 
 http://127.0.0.1:5000/convert/amount=10&src_currency=EUR&dest_currency=JPY&reference_date=2020-02-17
 for example this request will return the following json:
 
-'{
+```
+{
   "amount": 1190.5, 
   "currency": "JPY"
-}' 
+}
+```
 
 _it is important_ to format the reference date in this way (YYYY-MM-DD).
--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import requests
 import xmltodict
 
 app = Flask(__name__)
-class Currency(object):
-    def __init__(self,currency):
-        self.currency_list = currency
 
+# function that respond to the get request
 @app.route('/convert/amount=<amount>&src_currency=<src_currency>&dest_currency=<dest_currency>&reference_date=<reference_date>', methods = ['GET'])
 def get_converted_currency(amount,src_currency,dest_currency,reference_date):
     l = load_exchanges_rates()
-    src_curr_value, dest_curr_value = get_currencies(l,reference_date,src_currency,dest_currency)
-    current_value = (dest_curr_value/src_curr_value)*float(amount)
-    return jsonify({'amount': current_value, 'currency': dest_currency}),200
+    src_curr_value, dest_curr_value, found = get_currencies(l,reference_date,src_currency,dest_currency)
+    if found:
+        current_value = (dest_curr_value/src_curr_value)*float(amount)
+        return jsonify({'amount': current_value, 'currency': dest_currency}), 200
+    else:
+        return jsonify({"no available data for the required reference data {0}".format(reference_date)}), 404
 
-@app.route('/')
+
+# function that loads the exchange rates from the ecb site
 def load_exchanges_rates():
     url = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml'
     r = requests.get(url)
@@ -22,16 +24,24 @@ def load_exchanges_rates():
     l = d['gesmes:Envelope']['Cube']['Cube']
     return l
 
+#default home function
+@app.route('/')
+def greeting():
+    return "Welcome to the currency converter"
+
+# function that returns the currency rate values for the desired date and currencies
 def get_currencies(currency_list,reference_date,src_currency,dest_currency):
     print(reference_date,src_currency,dest_currency)
     src_currency_date = 1
     dest_currency_date = 1
+    found = False
     for i in range(len(currency_list)):
         if currency_list[i]['@time'] == str(reference_date):
+            found = True
             for item in currency_list[i]['Cube']:
                 if item['@currency'] == src_currency:
                     src_currency_date = item['@rate']
                 if item['@currency'] == dest_currency:
                     dest_currency_date = item['@rate']
 
-    return float(src_currency_date), float(dest_currency_date)
+    return float(src_currency_date), float(dest_currency_date), found
